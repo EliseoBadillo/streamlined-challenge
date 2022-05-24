ce9a355f7fb52f82568c165002dbf6253451a643
Eliseo Badillo <elseo.badillo@gmail.com>
2022-05-23 21:29:54 -0700

    Add Merchant resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Merchant.create(name: 'Merchant1')
+Merchant.create(name: 'Merchant2')
+Merchant.create(name: 'Merchant3')
